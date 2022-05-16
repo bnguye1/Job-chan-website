85edc6d0d4e31e2bb464d86761ed61dc306b4bfa
Brandon N <bnguyen4@umbc.edu>
2022-05-16 16:13:05 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# @app.route('/register', methods=['POST', 'GET'])
+# def register():
+#     if request.method == 'POST':
+#         # email = request.form['email']
+#         pass
+#
+#     else:
+#         pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
